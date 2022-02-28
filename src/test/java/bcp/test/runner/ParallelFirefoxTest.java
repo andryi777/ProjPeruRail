b13a7bc060c01b3d21f2009e925d189a59efc7a2
cmaurys <christian.maury@rimac.com.pe>
2022-02-28 01:26:45 -0500

    se añaden clases para browserstack

---
+++ src/test/java/bcp/test/runner/ParallelFirefoxTest.java
@@ -0,0 +1,15 @@
+package bcp.test.runner;
+
+import bcp.test.browserstack.BrowserStackSerenityTest;
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+
+
+@CucumberOptions(features = { "src/test/resources/features/" }, tags = { "@CompraTicketTrenBelmond" }, glue = {
+"bcp" }, plugin = { "pretty", "html:target/serenity-reports/serenity-html-report",
+		"json:target/serenity-reports/cucumber_report.json", "rerun:target/serenity-reports/rerun.txt" })
+public class ParallelFirefoxTest extends BrowserStackSerenityTest {
+}
---
+++ src/test/java/bcp/test/runner/SingleTest.java
@@ -0,0 +1,16 @@
+package bcp.test.runner;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+import bcp.test.browserstack.BrowserStackSerenityTest;
+
+@RunWith(CucumberWithSerenity.class)
+
+@CucumberOptions(features = { "src/test/resources/features/" }, tags = { "@CompraTicketTrenBelmond" }, glue = {
+"bcp" }, plugin = { "pretty", "html:target/serenity-reports/serenity-html-report",
+		"json:target/serenity-reports/cucumber_report.json", "rerun:target/serenity-reports/rerun.txt" })
+
+public class SingleTest extends BrowserStackSerenityTest {
+}
